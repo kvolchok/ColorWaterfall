426ea2dccc3a72f0c0d4f0c946d63bd54ab6cf30
kvolchok <kk.volchok@gmail.com>
2023-05-16 20:48:25 +0300

    refactor: changing field startColor to variable in class Cube

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -7,29 +7,26 @@ public class Cube : MonoBehaviour
     private float _recoloringDuration = 0.5f;
     
     private Material _cubeMaterial;
-    private Color _startColor;
-    
+
     private void Awake()
     {
         _cubeMaterial = GetComponent<MeshRenderer>().material;
-        _startColor = _cubeMaterial.color;
     }
 
     public IEnumerator ChangeColor(Color nextColor)
     {
+        var startColor = _cubeMaterial.color;
         var currentTime = 0f;
 
         while (currentTime <= _recoloringDuration)
         {
             var recoloringProgress = currentTime / _recoloringDuration;
 
-            var currentColor = Color.Lerp(_startColor, nextColor, recoloringProgress);
+            var currentColor = Color.Lerp(startColor, nextColor, recoloringProgress);
             _cubeMaterial.color = currentColor;
             currentTime += Time.deltaTime;
 
             yield return null;
         }
-        
-        _startColor = _cubeMaterial.color;
     }
 }
